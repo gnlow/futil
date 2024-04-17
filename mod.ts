5ec0f0857c6f16d0dabfb541b9c490670ebef847
Gnlow <gnlow@protonmail.com>
2024-04-17 10:43:50 +0900

    hmm

--- mod.ts
+++ mod.ts
@@ -1,21 +1,38 @@
 type Container<A> = {
     pipe<B>(f: (a: A) => B): Container<B>
     get(): A
+} & {
+    [ K in keyof A ]: Container<A[K]>
 }
 
 const container =
 <A>
 (a: A): Container<A> => {
-    return {
+    const target = {
         pipe<B>(f: (a: A) => B) {
             return container(f(a))
         },
         get() { return a }
     }
+    const proxy = new Proxy(target, {
+        get(target, prop, receiver) {
+            return 0
+                || target[prop as keyof typeof target]
+                || container(a[prop as keyof A])
+        }
+    }) as Container<A>
+
+    return proxy
 }
 
 console.log(
     container(1)
         .pipe(x => x * 2)
+        .get()
+)
+
+console.log(
+    container<String>("hello")
+        .length
         .get()
 )
